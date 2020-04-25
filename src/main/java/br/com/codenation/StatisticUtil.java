3ecafddd9796326b7ed875a7618978692cc3bd96
Montival Junior <montival_junior@yahoo.com.br>
2020-04-25 14:26:31 -0300

    Primeiro commit desafio aceleradev Java

---
+++ src/main/java/br/com/codenation/StatisticUtil.java
@@ -0,0 +1,19 @@
+package br.com.codenation;
+
+public class StatisticUtil {
+
+	public static int average(int[] elements) {
+
+		return 0;
+	}
+
+	public static int mode(int[] elements) {
+
+		return 0;
+	}
+
+	public static int median(int[] elements) {
+
+		return 0;
+	}
+}
