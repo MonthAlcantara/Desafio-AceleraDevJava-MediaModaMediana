7602854e28a3d327ffe65d83ce7f8048a9993b69
Montival Junior <montival_junior@yahoo.com.br>
2020-04-29 15:16:38 -0300

    Commit resolução desafio aceleradev Java

--- src/main/java/br/com/codenation/StatisticUtil.java
+++ src/main/java/br/com/codenation/StatisticUtil.java
@@ -1,10 +1,7 @@
 package br.com.codenation;
 
-import org.w3c.dom.stylesheets.LinkStyle;
-
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
